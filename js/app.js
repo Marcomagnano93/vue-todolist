a906eee6bd5206aeabfd3ab01c4fa89c612171cd
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-27 13:19:55 +0100

    ADD: html skeleton & bootstrap

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-       
+       toDoList: [
+        {
+            text: "Fai la spesa",
+            done: false
+        },
+        {
+            text: "Fai i compiti",
+            done: false
+        },
+        {
+            text: "Fatti la doccia",
+            done: false
+        },
+       ]
     }
   }
 }).mount('#app')
