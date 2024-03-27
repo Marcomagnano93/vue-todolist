6bb0c86e1922b6124b30d10963f88eb630e1ff99
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-27 15:30:43 +0100

    ADD: some style

--- js/app.js
+++ js/app.js
@@ -4,18 +4,18 @@ createApp({
   data() {
     return {
        toDoList: [
-        {
-            text: "Fai la spesa",
-            done: false
-        },
-        {
-            text: "Fai i compiti",
-            done: false
-        },
-        {
-            text: "Fatti la doccia",
-            done: false
-        },
+        // {
+        //     text: "Fai la spesa",
+        //     done: false
+        // },
+        // {
+        //     text: "Fai i compiti",
+        //     done: false
+        // },
+        // {
+        //     text: "Fatti la doccia",
+        //     done: false
+        // },
        ],
        inputValue: ""
     }
